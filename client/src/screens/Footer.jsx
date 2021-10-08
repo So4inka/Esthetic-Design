ea8652e52962160a0abd6d4b359fed58e7029d2e
so4inka <kallaurchik@gmail.com>
2021-10-08 15:38:31 -0400

    needed to run Gemfile again

---
+++ client/src/screens/Footer.jsx
@@ -0,0 +1,26 @@
+import "./Footer.css";
+
+function Footer() {
+
+  return (
+    <div className="footer">
+      
+      <footer className="footer">
+        <a href="https://github.com/So4inka">
+          <img src="https://res.cloudinary.com/briandanger/image/upload/v1568954107/github_fpykxh.png" />
+        </a>
+        <a href="https://www.linkedin.com/in/nadezda-kallaur-65444a85/">
+          <img src="https://i.imgur.com/bj7hAbc.png" />
+        </a>
+        <a href="https://www.instagram.com/nadia_kallaur/">
+          <img src="https://i.imgur.com/OZawu9A.png" />
+        </a>
+        <a href="https://www.facebook.com/nadune4ka/">
+          <img src="https://i.imgur.com/AmFK9On.png" />
+        </a>
+      </footer>
+ </div>
+  )
+}
+
+export default Footer;
---
+++ client/src/screens/Home.jsx
@@ -0,0 +1,16 @@
+import './Home.css';
+import Layout from './layouts/Layout';
+import Footer from './Footer'
+
+
+const Home = (props) => {
+  return (
+<div
+
+
+
+
+
+  )
+}
+ 
